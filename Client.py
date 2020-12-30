1434282327939987bbc76745f4401e8a2faff1a9
MosheBiran <73993888+MosheBiran@users.noreply.github.com>
2020-12-30 19:07:32 +0200

    123

--- Client.py
+++ Client.py
@@ -1,51 +1,87 @@
 import msvcrt
 import socket
+import time
 from threading import Thread
+
+# Linux
+# import sys
+# import select
+# import tty
+# import termios
+#
+#
+# def isData():
+#     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
 
 def main():
-
     while True:
-        # UDP
-        try:
-            correctDate = b'\xfe\xed\xbe\xef\x02'
-            ip = '127.0.0.1'
-            port = 13117
-            client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-            client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            client.bind(("", port))
-            print("Client started, listening for offer requests...")
-            data, address = client.recvfrom(1024)
-            if data[0:4] != correctDate[0:4]:
-                print("Wrong Message")
-                # continue
-            print("Received offer from " + ip + "," + " attempting to connect...")
-        except:
-            print("Oh NO!!!!")
+        while True:
+            # UDP
+            try:
+                correctDate = b'\xfe\xed\xbe\xef\x02'
+                ip = '127.0.0.1'
+                port = 13117
+                client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+                client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                client.bind(("", port))
+                print("Client started, listening for offer requests...")
+                data, address = client.recvfrom(1024)
+                if data[0:4] != correctDate[0:4]:
+                    print("Wrong Message")
+                    # continue
+                print("Received offer from " + ip + "," + " attempting to connect...")
+            except:
+                print("Oh NO!!!!")
 
 
-        group_name = "Bits please, this is not a bug it's a feature\n"
+            group_name = "Bits please, this is not a bug it's a feature\n"
 
-        # TCP
-        try:
-            serverName = '127.0.0.1'
-            clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            clientSocket.connect((serverName, (int(hex(data[5])[2:]+hex(data[6])[2:], 16))))
-            clientSocket.sendall(bytes(group_name, 'utf-8'))
-            break
-            # clientSocket.close()
-        except:
-            print("Oh NO TCP Problem!!!!")
+            # TCP
+            try:
+                serverName = '127.0.0.1'
+                clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                clientSocket.connect((serverName, (int(hex(data[5])[2:]+hex(data[6])[2:], 16))))
+                clientSocket.sendall(bytes(group_name, 'utf-8'))
+                break
+                # clientSocket.close()
+            except:
+                print("Oh NO TCP Problem!!!!")
 
-    message = clientSocket.recv(1024)
-    print(message.decode('utf-8'))
+        message = clientSocket.recv(1024)
+        print(message.decode('utf-8'))
 
-    while True:
-        if msvcrt.kbhit():
-            key = msvcrt.getch()
-            clientSocket.send(key)
-            print("Key is {}".format(key))
+        #  Windows
+        now = time.time()
+        futureTCP = now + 10
+        while time.time() < futureTCP:
+            if msvcrt.kbhit():
+                key = msvcrt.getch()
+                clientSocket.send(key)
+                print("Key is {}".format(key))
 
+
+        # #  Linux!
+        # old_settings = termios.tcgetattr(sys.stdin)
+        # try:
+        #     tty.setcbreak(sys.stdin.fileno())
+        #
+        #     i = 0
+        #     while 1:
+        #         print(i)
+        #         i += 1
+        #
+        #         if isData():
+        #             c = sys.stdin.read(1)
+        #             if c == '\x1b':  # x1b is ESC
+        #                 break
+        #
+        # finally:
+        #     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+        message = clientSocket.recv(1024)
+        print(message.decode('utf-8'))
+        clientSocket.close()
 
 
 
--- Server.py
+++ Server.py
@@ -3,38 +3,27 @@ import socket
 import time
 from threading import Thread
 from time import sleep
+from scapy.all import get_if_addr
 
 
-
-def brodcast():
+def brodcast(socky):
     try:
         message = b'\xfe\xed\xbe\xef\x02\x19\xA5'
 
         ip = '127.0.0.1'
-        port = 13117
-        # UDP
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.bind((ip, port))
-        # mode of listening
         print("Server started, listening on IP address " + ip)
         # our message is "offer" to every client who is listening
         now = time.time()
         future = now + 10
         while time.time() < future:
-            sock.sendto(message, ('255.255.255.255', 13117))
+            socky.sendto(message, ('255.255.255.255', 13117))
             sleep(1)
     except:
         print("Oh NO!!!!")
 
 
-def receiveTCP(players):
-    ip = '127.0.0.1'
-    serverPort = 6565
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind((ip, serverPort))
-    serverSocket.listen(20)
+def receiveTCP(players, serverSocket):
+
 
     nowTCP = time.time()
     futureTCP = nowTCP + 10
@@ -78,12 +67,6 @@ def makeMessageToGroups(group1, group2):
     return message
 
 
-# TODO - make remove
-def sendMessageToGroups(players, message):
-    for playerSock in players.values():
-        playerSock.sendall(bytes(message, 'utf-8'))
-
-
 def startGame(groupCount, playerSocket, message):
     playerSocket.sendall(bytes(message, 'utf-8'))
 
@@ -92,43 +75,90 @@ def startGame(groupCount, playerSocket, message):
     while time.time() < futureTCP:
         # connectionSocket, address = playerSocket.accept()
         sentence = playerSocket.recv(1024)
-        print(sentence.decode('utf-8'))  # TODO - Remove
+        # print(sentence.decode('utf-8'))  # TODO - Remove
         if sentence.decode('utf-8') not in groupCount:
             groupCount[sentence.decode('utf-8')] = 1
         else:
             groupCount[sentence.decode('utf-8')] += 1
 
 
+def gameSummaryMessage(group1, group2, group1Count, group2Count):
+    winners = {}
+    message = ""
+    sumOfGroup1 = sum(group1Count.values())
+    sumOfGroup2 = sum(group2Count.values())
+    message += "**************\n"
+    message += "* Game over! *\n"
+    message += "**************\n"
+    message += "Group 1 typed in " + str(sumOfGroup1) + " characters.\n"
+    message += "Group 2 typed in " + str(sumOfGroup2) + " characters.\n"
+    if sumOfGroup1 > sumOfGroup2:
+        message += "Group 1 wins!\n\n"
+        winners = group1
+    elif sumOfGroup1 < sumOfGroup2:
+        message += "Group 2 wins!\n\n"
+        winners = group2
+    elif sumOfGroup1 == sumOfGroup2:
+        message += "Its a TIE!!!!! Good Job!\n\n"
+        # TODO - add 1 and 2
+
+    message += "Congratulations to the winners:\n\n"
+    message += "==\n\n"
+    for name in winners.keys():
+        message += name + "\n\n"
+
+    return message
 
 
 def main():
-    players = {}
 
-    _thread.start_new_thread(brodcast, ())
-    _thread.start_new_thread(receiveTCP, (players, ))
-    sleep(10.1)
+    # ip = get_if_addr('eth1')
 
-    lst = divedToGroups(players)
-    group1 = lst[0]
-    group2 = lst[1]
-
-    group1Count = {}
-    group2Count = {}
-    message = makeMessageToGroups(group1, group2)
-    # sendMessageToGroups(players, message)  # TODO - maybe in thread?
-
-    for teamName, playerSocket in players.items():
-        if teamName in group1:
-            _thread.start_new_thread(startGame, (group1Count, playerSocket, message, ))
-        else:
-            _thread.start_new_thread(startGame, (group2Count, playerSocket, message, ))
-
-    sleep(10.1)
-
-    print(sum(group1Count.values()))
-    print(sum(group2Count.values()))
+    # UDP SOCKET
+    ip = '127.0.0.1'
+    port = 13117
+    # UDP
+    socky = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socky.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socky.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    socky.bind((ip, port))
+    # mode of listening
 
 
+    # TCP SOCKET
+    ip = '127.0.0.1'
+    serverPort = 6565
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((ip, serverPort))
+    serverSocket.listen(20)
+
+    while True:
+        players = {}
+
+        _thread.start_new_thread(brodcast, (socky, ))
+        _thread.start_new_thread(receiveTCP, (players, serverSocket, ))
+        sleep(10.1)
+
+        lst = divedToGroups(players)
+        group1 = lst[0]
+        group2 = lst[1]
+
+        group1Count = {}
+        group2Count = {}
+        message = makeMessageToGroups(group1, group2)
+
+        for teamName, playerSocket in players.items():
+            if teamName in group1:
+                _thread.start_new_thread(startGame, (group1Count, playerSocket, message, ))
+            else:
+                _thread.start_new_thread(startGame, (group2Count, playerSocket, message, ))
+
+        sleep(10.1)
+
+        message = gameSummaryMessage(group1, group2, group1Count, group2Count)
+
+        for playerSocket in players.values():
+            playerSocket.sendall(bytes(message, 'utf-8'))
 
 
 
